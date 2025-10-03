4d0d61a30d72c9b3cbd5e7105bf7416de287e213
Aidan Sullivan <aidsully@iastate.edu>
2025-10-02 23:48:49 -0500

    update

--- main.cpp
+++ main.cpp
@@ -11,14 +11,29 @@ color ray_color(const ray &r)
 
 int main()
 {
-    auto aspect_ratio = 16.0 / 9.0;
+    double aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
+    int image_height = image_width / aspect_ratio;
+    if (image_height < 1)
+        image_height = 1;
 
-    // Calculate the image height, and ensure that it's at least 1.
-    int image_height = int(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
-
-    // Viewport widths less than one are ok since they are real valued.
     auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width) / image_height);
+    auto viewport_width = viewport_height * (image_width / image_height);
+
+    vec3 camera_center = vec3(0, 0, 0);
+    auto focal_length = 1.0;
+
+    // FILE OUTPUT
+    std::cout << "P3" << std::endl;
+    std::cout << image_width << " " << image_height << std::endl;
+    std::cout << 255 << std::endl;
+    for (int i = 0; i < image_height; i++)
+    {
+        std::clog << "\rRows Remaining: " << image_height - i << std::flush;
+        for (int j = 0; j < image_width; j++)
+        {
+            std::cout << color(255, 255, 255) << std::endl;
+        }
+    }
+    std::clog << "\rDONE! \n";
 }
