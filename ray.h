3f80200d1cd4832c1491752d8711652902a8b7e8
Aidan Sullivan <aidan.a.sullivan@gmail.com>
2025-12-10 19:40:33 -0600

    added motion blur

--- ray.h
+++ ray.h
@@ -6,17 +6,27 @@ class ray
 public:
     vec3 origin;
     vec3 direction;
+    double time;
 
     ray()
     {
         origin = vec3(0, 0, 0);
         direction = vec3(0, 0, 0);
+        time = 0.0;
     }
 
     ray(vec3 vecA, vec3 vecB)
     {
         origin = vecA;
         direction = vecB;
+        time = 0.0;
+    }
+
+    ray(vec3 vecA, vec3 vecB, double num)
+    {
+        origin = vecA;
+        direction = vecB;
+        time = num;
     }
 
     vec3 at(double t) const
